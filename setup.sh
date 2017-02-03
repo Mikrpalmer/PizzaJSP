3a0cb17e3e7a77a6cbab2c188e323cc3baf9f23a
Mikrpalmer <Palmermr20@mounties.mansfield.edu>
2017-02-02 21:00:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
-
+#!/bin/bash
+date
 apt-get -qq update && apt-get -qq -V upgrade
-apt-get install locate mlocate
+apt-get -qq install locate mlocate
 updatedb
-apt-get install ntp
-apt-get install apache2 apache2-mpm-prefork
+apt-get -qq install -y ntp
+apt-get -qq install -y apache2 apache2-mpm-prefork
 systemctl restart apache2.service
-apt-get install php5
-apt-get install php5-mysqlnd
-apt-get install php5-gd
-apt-get install php5-curl
-apt-get install git
-apt-get install s3cmd
-apt-get install mc p7zip-full htop
-apt-get install debian-goodies
+apt-get -qq install -y php5
+apt-get -qq install -y php5-mysqlnd
+apt-get -qq install -y php5-gd
+apt-get -qq install -yphp5-curl
+apt-get -qq install -y git
+apt-get -qq install -y s3cmd
+apt-get -qq install -y mc p7zip-full htop
+apt-get -qq install -y debian-goodies
+apt-get -qq install -y rkhunter
 checkrestart
 apt-get update && apt-get -V upgrade
 updatedb
